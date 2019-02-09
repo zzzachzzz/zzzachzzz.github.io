23e14374f184d6a11c8f5c76ff8118907e5d3fde
zzzachzzz <zach1rosen@gmail.com>
2019-02-09 22:40:26 +0000

    Added tech logos, further styling of project cards.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,32 @@ class App extends Component {
   constructor() {
     super()
     this.projectsRef = React.createRef();
+    this.projects = {
+      0: {
+        title: 'AutomateMyJob',
+        body: `Developed a program that utilizes Selenium Webdriver to automate interactions with a web app used to
+            build web pages. It is used in conjunction with the Google Sheets API to parse for the necessary
+information to build a page, and translate that into a sequence of actions. The functions are run in a
+Python interactive shell, allowing for a hybrid workflow of manual and automated work in a single
+browser session.`,
+        icons: ['python.png', 'selenium.png', 'google_sheets.png']
+      },
+      1: {
+        title: 'Toonkit.net',
+        body: '',
+        icons: ['python.png', 'flask.png', 'postgreSQL.png', 'digital_ocean.png']
+      },
+      2: {
+        title: 'LoLWikiQuotes',
+        body: '',
+        icons: ['python.png', 'beautiful_soup.png', 'riot_games_api.png']
+      },
+      3: {
+        title: 'Portfolio',
+        body: '',
+        icons: ['javascript.png', 'react.png', 'digital_ocean.png']
+      }
+    }
   }
 
   scroll = (ref) => {
@@ -30,11 +56,11 @@ class App extends Component {
         <h2 style={{textAlign: 'center'}}>Projects</h2>
         <div className="ubuntu-card-background">
           <div className="cards-container">
-            <ProjectCard />
-            <ProjectCard />
-            <ProjectCard />
-            <ProjectCard />
-            <ProjectCard />
+            <ProjectCard project={this.projects[0]} />
+            <ProjectCard project={this.projects[1]} />
+            <ProjectCard project={this.projects[2]} />
+            <ProjectCard project={this.projects[3]} />
+            {/* <ProjectCard /> */}
           </div>
         </div>
       </div>  
--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,12 +1,16 @@
 import React, {memo} from 'react';
+import './ProjectCard.css';
 
-function ProjectCard({}) {
+function ProjectCard(props) {
+  const {title, body, icons} = props.project;
   return (
     <div className="outer">
       <div className="inner">
-        <h2>Project Title</h2>
-        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim</p>
-        <div className="tech-icon-row">tech-icon-row</div>
+        <h2>{title}</h2>
+        <p>{body}</p>
+        <div className="tech-icon-row">
+          { icons.map((icon) => {return <img src={require(`../assets/${icon}`)} className="tech-icon" />}) }
+        </div>
       </div>
     </div>
   );
