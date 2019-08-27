25cfd8c581ffeb45cbf8f33891cd3b2a6873d722
zzzachzzz <zach1rosen@gmail.com>
2019-08-27 21:32:50 +0000

    Revert css change - problem was import order.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import './App.css';
 
 import Portfolio from './portfolio/Portfolio';
 import Blog from './blog/Blog';
