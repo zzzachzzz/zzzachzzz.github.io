b3f72457d5bb71cda423667a1a258b9697a4d278
zach1rosen <zach1rosen@gmail.com>
2019-08-27 15:02:30 -0600

    Use index.css intead of App.css - doesn't work the same after npm build

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Portfolio from './portfolio/Portfolio';
 import Blog from './blog/Blog';
-import './App.css';
 
 export default () => (
   <BrowserRouter>
