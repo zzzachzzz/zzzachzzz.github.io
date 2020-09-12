278ee61b43e4921b6af8929667aa0cfff1f44987
zzzachzzz <zach1rosen@gmail.com>
2020-09-12 09:28:55 -0600

    Only use Google Analytics component in production

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,12 +8,18 @@ import Blog from './blog/Blog';
 
 export default () => (
   <BrowserRouter>
-    <GAListener trackingId="UA-146825130-1">
-      <Switch>
-        <Route exact path="/" component={Portfolio} />
-        <Route path="/blog" component={Blog} />
-      </Switch>
-    </GAListener>
+    {process.env.NODE_ENV === 'production'
+      ? <GAListener trackingId="UA-146825130-1"><Routes /></GAListener>
+      : <Routes />
+    }
   </BrowserRouter>
+);
+
+const Routes = () => (
+  <Switch>
+    <Route exact path="/test" component={Sandbox} />
+    <Route exact path="/" component={Portfolio} />
+    <Route path="/blog" component={Blog} />
+  </Switch>
 );
 
