138f3fcee99f5a8746a8129bcd7582919c15bae9
zzzachzzz <zach1rosen@gmail.com>
2021-02-13 16:16:28 -0700

    Show actual 404 page for /blog/edit

--- pages/blog/edit/index.tsx
+++ pages/blog/edit/index.tsx
@@ -1,4 +1,6 @@
+import Custom404 from '@/pages/404';
+
 // Only exists to prevent /blog/edit (no [slug]) from being a valid path
 export default function _404this() {
-  return null;
+  return <Custom404 />;
 }
