0c94ce0784bc784a9f1be5e997001d13fe3388f1
zzzachzzz <zach1rosen@gmail.com>
2025-12-13 06:00:01 -0700

    Redirects for paths with trailing slashes to not 404
    Why is that not default behavior wtf

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
+// @ts-check
+
+/** @type {import('next').NextConfig} */
 export default {
   experimental: {
     scrollRestoration: true,
   },
+  redirects: async () => [
+    {
+      source: '/:path+/',
+      destination: '/:path+',
+      permanent: true,
+    },
+  ],
 }
